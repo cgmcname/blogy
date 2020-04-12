d244f1247861df2f689a177764e8b772f0db5a9f
cgmcname <periclescgm@live.com>
2020-04-12 15:29:16 -0500

    Implement database connection

--- djreact/settings.py
+++ djreact/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'djreact.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} """
 
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
