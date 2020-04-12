06797b13bae806008d795ac44eb56a23b0f8beab
cgmcname <periclescgm@live.com>
2020-04-12 16:31:40 -0500

    clean up nav

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -11,9 +11,9 @@ const CustomLayout = (props) => {
             <Header>
             <div className="logo" />
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                <Menu.Item key="1">nav 1</Menu.Item>
+              {/*   <Menu.Item key="1">nav 1</Menu.Item>
                 <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item> */}
             </Menu>
             </Header>
             <Content style={{ padding: '0 50px' }}>
