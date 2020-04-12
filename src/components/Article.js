f3fac79ce65f43ba91e6803aab57df36ed5cd08d
cgmcname <periclescgm@live.com>
2020-04-12 16:47:35 -0500

    0

--- src/components/Article.js
+++ src/components/Article.js
@@ -28,9 +28,9 @@ const Articles = (props) => {
             <List.Item
                 key={item.title}
                 actions={[
-                <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                <IconText icon={StarOutlined} text="0" key="list-vertical-star-o" />,
+                <IconText icon={LikeOutlined} text="0" key="list-vertical-like-o" />,
+                <IconText icon={MessageOutlined} text="0" key="list-vertical-message" />,
                 ]}
                 extra={
                 <img
